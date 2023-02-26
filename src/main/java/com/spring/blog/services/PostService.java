027a89ba371b2a8de5cf29e2e14101ed8e5a0e2b
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-26 15:38:23 -0300

    criação da interface PostService

---
+++ src/main/java/com/spring/blog/services/PostService.java
@@ -0,0 +1,14 @@
+package com.spring.blog.services;
+
+import java.util.List;
+
+import com.spring.blog.models.Post;
+
+public interface PostService {
+
+    List<Post> findAll();
+
+    Post findById(long id);
+
+    Post save(Post obj);
+}
