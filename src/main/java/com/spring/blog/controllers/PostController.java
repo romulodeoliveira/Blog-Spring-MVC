9581a26432c97b3db08035e6efa343520930e9aa
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-26 22:39:19 -0300

    configuração do metodo POST

--- src/main/java/com/spring/blog/controllers/PostController.java
+++ src/main/java/com/spring/blog/controllers/PostController.java
@@ -1,15 +1,21 @@
 package com.spring.blog.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.blog.models.Post;
 import com.spring.blog.services.PostService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PostController {
@@ -23,6 +29,29 @@ public class PostController {
         List<Post> posts = postService.findAll();
         mv.addObject("posts", posts);
         return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = postService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        postService.save(post);
+        return "redirect:/posts";
     }
 
 }
