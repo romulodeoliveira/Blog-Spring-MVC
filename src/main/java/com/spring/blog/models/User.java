545d6928d6d9351f9b411e39585154499b65fdfb
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-01 00:39:41 -0300

    mudança em algumas colunas da tabela

--- src/main/java/com/spring/blog/models/User.java
+++ src/main/java/com/spring/blog/models/User.java
@@ -1,23 +1,31 @@
 package com.spring.blog.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "tb_user")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -26,7 +34,11 @@ public class User {
 
     @Column(nullable = false, unique = true)
     @NotBlank
-    private String login;
+    private String userName;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
 
     @Column(nullable = false)
     @NotBlank
@@ -42,9 +54,19 @@ public class User {
     @NotBlank
     private String email;
 
-    @Column(nullable = false)
-    @NotBlank
-    private String password;
+    private boolean superUser = false;
+
+    /**
+     * A lista roles geralmente é utilizada em sistemas de autenticação e controle
+     * de acesso para determinar quais permissões um determinado usuário possui
+     * dentro do sistema. Cada elemento da lista representa uma permissão
+     * específica, como por exemplo "ADMIN", "USER", "EDITOR", etc.
+     */
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<String> roles = new ArrayList<>();
+
+    // Encriptação de senhas
 
     public void setPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
