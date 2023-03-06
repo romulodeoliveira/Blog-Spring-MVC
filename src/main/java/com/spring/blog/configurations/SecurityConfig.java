86f2b524c832ccad1b6235911cae693b3b3aeb79
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-06 10:35:03 -0300

    configuração de seguraça com SecurityFilterChain, RememberMeServices e BCryptPasswordEncoder

--- src/main/java/com/spring/blog/configurations/SecurityConfig.java
+++ src/main/java/com/spring/blog/configurations/SecurityConfig.java
@@ -2,30 +2,90 @@ package com.spring.blog.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices.RememberMeTokenAlgorithm;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-        @Bean
-        public WebSecurityCustomizer webSecurityCustomizer() {
-                return (web) -> web.ignoring().requestMatchers("/posts/**")
-                                .requestMatchers(new AntPathRequestMatcher("/posts"));
-        }
+        // Session Management Migrations
+        // https://docs.spring.io/spring-security/reference/migration/servlet/session-management.html
+
+        // Authentication Migrations
+        // https://docs.spring.io/spring-security/reference/migration/servlet/authentication.html
+
+        /**
+         * securityFilterChain cria e retorna um filtro de segurança do Spring Security.
+         * Esse filtro é adicionado à cadeia de filtros que processam as requisições
+         * HTTP recebidas pela aplicação. O filtro é definido usando a classe
+         * HttpSecurity, que permite a configuração das políticas de segurança para
+         * diferentes camadas da aplicação web, como autenticação, autorização e
+         * controle de sessão.
+         */
+
+        /**
+         * o serviço rememberMeServices é configurado através da chamada ao método
+         * rememberMe, que permite especificar as opções de configuração do serviço de
+         * "lembrar-me".
+         */
+
+        /**
+         * A classe TokenBasedRememberMeServices é uma classe do Spring Security que
+         * implementa a interface RememberMeServices. Geralmente, ela é importada de uma
+         * das dependências do Spring Security.
+         */
+
+        /**
+         * A classe TokenBasedRememberMeServices é uma implementação do serviço
+         * RememberMeServices que usa um token criptografado para manter o usuário
+         * logado. O construtor dessa classe recebe uma chave privada e um
+         * UserDetailsService como argumentos.
+         */
 
         @Bean
-        public InMemoryUserDetailsManager userDetailsService() {
-                UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("user")
-                                .password("password")
-                                .roles("USER")
-                                .build();
-                return new InMemoryUserDetailsManager(user);
+        SecurityFilterChain securityFilterChain(HttpSecurity http, RememberMeServices rememberMeServices)
+                        throws Exception {
+                http
+                                // ...
+                                .rememberMe((remember) -> remember
+                                                .rememberMeServices(rememberMeServices));
+                return http.build();
         }
+
+        /**
+         * rememberMeServices cria um serviço para manter o usuário logado na aplicação
+         * web, mesmo depois que ele fecha o navegador ou reinicia o computador. O
+         * serviço RememberMeServices é uma interface do Spring Security que deve ser
+         * implementada para gerenciar o comportamento de "lembrar-me" da aplicação.
+         */
+
+        @Bean
+        RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+                RememberMeTokenAlgorithm encodingAlgorithm = RememberMeTokenAlgorithm.SHA256;
+                TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices(Constantes.myKey,
+                                userDetailsService,
+                                encodingAlgorithm);
+                rememberMe.setMatchingAlgorithm(RememberMeTokenAlgorithm.SHA256);
+                return rememberMe;
+        }
+
+        /**
+         * O BCryptPasswordEncoder é um algoritmo de codificação de senhas do Spring
+         * Security que utiliza a função de hash bcrypt para proteger as senhas dos
+         * usuários. Ele é recomendado porque é seguro e fornece um bom nível de
+         * proteção para as senhas armazenadas.
+         */
+
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
 }
